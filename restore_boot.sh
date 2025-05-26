3bb664a982ad531ff9957fffc2ab5084a8fbfc13
gates-alt <gatestechalt@outlook.com>
2025-05-26 18:21:11 -0400

    `restore_boot.sh` added
    use this if your phone don't boots after trying to unlock the bootloader

---
+++ restore_boot.sh
@@ -0,0 +1,24 @@
+termux-wake-lock
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WORKSPACE_DIR="$SCRIPT_DIR/workspace"
+APPS_DIR="$SCRIPT_DIR/apps"
+BIN_DIR="$SCRIPT_DIR/bin"
+UBOOT_DIR="$SCRIPT_DIR/uboot_spl"
+
+if [ ! -d "$WORKSPACE_DIR" ]; then
+    mkdir -p "$WORKSPACE_DIR"
+fi
+
+cd "$WORKSPACE_DIR"
+
+run_cmd() {
+    while true; do
+        termux-usb -l > file.txt || continue
+        USB_DEVICE=$(grep -o /dev/bus/usb/[0-9]*/[0-9]* file.txt)
+        termux-usb -r $USB_DEVICE || continue
+        termux-usb -e "$APPS_DIR/spd_dump --usb-fd $1" $USB_DEVICE && break
+    done
+}
+
+run_cmd "loadexec $BIN_DIR/custom_exec_no_verify_65015f08.bin fdl $BIN_DIR/fdl1-dl.bin 0x65000800 fdl $BIN_DIR/fdl2-dl.bin 0x9efffe00 exec w splloader $UBOOT_DIR/u-boot-spl-16k-ufs-sign.bin w uboot $UBOOT_DIR/lk-sign.bin reset"
